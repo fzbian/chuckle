2a7ad44c258ea70b808e06259c1586fae9c344a7
Fabian Martin <hello@fzbian.me>
2023-02-03 20:50:24 -0500

    feat: new bot info command (1.2)

---
+++ src/libs/interactions/info.js
@@ -0,0 +1,28 @@
+const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
+const { MainColor, DiscordInviteLink, BotName } = require('../config');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+  .setName('info')
+  .setDescription('Displays bot information.'),
+  async execute(interaction) {
+
+    const info = new EmbedBuilder()
+    info.setTitle(`${BotName} | A meme factory`)
+    info.setColor(MainColor)
+    info.setAuthor({ name: `Requested by ${interaction.user.username}`, iconURL: interaction.user.avatarURL() })
+    info.setTimestamp()
+    .addFields(
+      { name: 'Server', value: 'Oxta.Cloud', inline: true },
+      { name: 'Library', value: 'Discord.js', inline: true },
+      { name: 'Developer', value: 'fzbian#9210', inline: true },
+    )
+    info.setAuthor({
+      name: "Invite bot",
+      url: DiscordInviteLink
+    })
+    info.setFooter({ text: "Bot info", iconURL: interaction.guild.iconURL() })
+  
+    await interaction.reply({embeds: [info]})
+  }
+}
