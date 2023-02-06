dd1ad54a6a2ec0707beedf9ffe7896846b333100
Fabian Martin <hello@fzbian.me>
2023-02-05 21:47:59 -0500

    feat: send message when in guildDelete

--- src/libs/events/guildDelete.js
+++ src/libs/events/guildDelete.js
@@ -1,5 +1,19 @@
-const logger = require('../logger');
+const { EmbedBuilder } = require("discord.js");
+const { LoggerChannel, RedColor } = require('../config');
 
 module.exports = async (guild) => {
-    logger.info(`Bot removed from ${guild.name} (${guild.id}).`)
+    const channel = guild.client.channels.cache.get(LoggerChannel);
+
+    const logger = new EmbedBuilder()
+    logger.setColor(RedColor)
+    logger.setTitle("Bot removed")
+    logger.addFields(
+		{ name: 'Name', value: `${guild.name}`, inline: true },
+        { name: 'ID', value: `${guild.id}`, inline: true  },
+        { name: 'Members', value: `${guild.memberCount}`, inline: true  },
+        { name: 'Owner ID', value: `${guild.ownerId}`, inline: true  },
+	)
+    logger.setThumbnail(guild.iconURL())
+    logger.setFooter({ text: "Logger", iconURL: guild.iconURL() })
+    channel.send({embeds: [logger]})
 };
