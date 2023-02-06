d5a0457e6928c10c3a32a66b22f216fbcd283836
Fabian Martin <hello@fzbian.me>
2023-02-06 18:47:39 -0500

    refactor: independent information for the developer in info

--- src/libs/interactions/info.js
+++ src/libs/interactions/info.js
@@ -6,30 +6,43 @@ module.exports = {
     .setName('info')
     .setDescription('Displays bot information.'),
   async execute(interaction) {
-    if (interaction.user.id == DeveloperID) {
-      
-      interaction.client.guilds.cache.size
-      await interaction.reply(`Servers: ${interaction.client.guilds.cache.size}`)
-    } else {
-      const Buttons = new ActionRowBuilder()
-      Buttons.addComponents(
-        new ButtonBuilder()
-          .setLabel('Invite me!')
-          .setURL(DiscordInviteLink)
-          .setStyle(ButtonStyle.Link),
-      );
+    const Buttons = new ActionRowBuilder()
+    Buttons.addComponents(
+      new ButtonBuilder()
+        .setLabel('Invite me!')
+        .setURL(DiscordInviteLink)
+        .setStyle(ButtonStyle.Link),
+    );
 
-      const info = new EmbedBuilder()
-      info.setTitle(`${BotName} | A meme factory`)
-      info.setAuthor({ name: `Requested by ${interaction.user.username}`, iconURL: interaction.user.avatarURL() })
-      info.setColor(MainColor)
-      info.addFields(
-        { name: 'Server', value: 'Oxta.Cloud', inline: true },
-        { name: 'Library', value: 'Discord.js', inline: true },
-        { name: 'Developer', value: 'fzbian#9210', inline: true },
-      )
-      info.setTimestamp()
-      info.setFooter({ text: "Bot info", iconURL: interaction.guild.iconURL() })
+    const info = new EmbedBuilder()
+    info.setTitle(`${BotName} | A meme factory`)
+    info.setAuthor({ name: `Requested by ${interaction.user.username}`, iconURL: interaction.user.avatarURL() })
+    info.setColor(MainColor)
+    info.addFields(
+      { name: 'Server', value: 'Oxta.Cloud', inline: true },
+      { name: 'Library', value: 'Discord.js', inline: true },
+      { name: 'Developer', value: 'fzbian#9210', inline: true },
+    )
+    info.setTimestamp()
+    info.setFooter({ text: "Bot info", iconURL: interaction.guild.iconURL() })
+
+    const DevInfo = new EmbedBuilder()
+    DevInfo.setTitle(`${BotName} | A meme factory`)
+    DevInfo.setAuthor({ name: `Requested by ${interaction.user.username}`, iconURL: interaction.user.avatarURL() })
+    DevInfo.setColor(MainColor)
+    DevInfo.addFields(
+      { name: 'Server', value: 'Oxta.Cloud', inline: true },
+      { name: 'Library', value: 'Discord.js', inline: true },
+      { name: 'Developer', value: 'fzbian#9210', inline: true },
+      { name: '\u200B', value: '**Developer Info**\u200B' },
+      { name: 'Servers', value: `${interaction.client.guilds.cache.size}`, inline: true },
+    )
+    info.setTimestamp()
+    info.setFooter({ text: "Bot info", iconURL: interaction.guild.iconURL() })
+
+    if (interaction.user.id == DeveloperID) {
+      await interaction.reply({ embeds: [DevInfo] })
+    } else {
       await interaction.reply({ embeds: [info], components: [Buttons] })
     }
   }
