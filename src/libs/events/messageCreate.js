48500c77d88ee3a325bf2498cc023b3a2e329700
Fabian Martin <hello@fzbian.me>
2023-02-05 21:48:32 -0500

    feat: sends message when bot is used

--- src/libs/events/messageCreate.js
+++ src/libs/events/messageCreate.js
@@ -1,7 +1,21 @@
-const logger = require("../logger");
+const { EmbedBuilder } = require("discord.js");
+const { LoggerChannel, MainColor } = require('../config');
 
 module.exports = (e) => {
-    logger.info(`MessageCreate interaction for user [discordSnowflake=${e.author.id}]`);
+    if (e.interaction && e.interaction.user) {
+        const channel = e.client.channels.cache.get(LoggerChannel);
 
-    if (e.author.bot === true) return;
+        const logger = new EmbedBuilder()
+        logger.setColor(MainColor)
+        logger.setAuthor({
+            name: `${e.interaction.user.username}#${e.interaction.user.discriminator}`,
+            iconURL: e.interaction.user.avatarURL()
+        })
+        logger.addFields(
+            { name: 'Command', value: e.interaction.commandName, inline: true },
+            { name: 'Guild ID', value: e.guildId, inline: true },
+        )
+        logger.setFooter({ text: "Logger" })
+        channel.send({ embeds: [logger] })
+    }
 };
