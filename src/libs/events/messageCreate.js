b46b2611f6de31505d955a1094d1b45e9bc71c33
Fabian Martin <hello@fzbian.me>
2023-02-06 18:39:30 -0500

    fix: only records bot commands
    previously the commands that used slashcommands from discord.js were recorded independently of the bot, now it will do it only with our bot.

--- src/libs/events/messageCreate.js
+++ src/libs/events/messageCreate.js
@@ -1,21 +1,25 @@
 const { EmbedBuilder } = require("discord.js");
-const { InteractionChannel, MainColor } = require('../config');
+const { InteractionChannel, MainColor, DiscordClientId } = require('../config');
 
 module.exports = (e) => {
-    if (e.interaction && e.interaction.user) {
-        const channel = e.client.channels.cache.get(InteractionChannel);
+    if (String(e.author.id) !== DiscordClientId) {
+        return null;
+    } else {
+        if (e.interaction && e.interaction.user) {
+            const channel = e.client.channels.cache.get(InteractionChannel);
 
-        const logger = new EmbedBuilder()
-        logger.setColor(MainColor)
-        logger.setAuthor({
-            name: `${e.interaction.user.username}#${e.interaction.user.discriminator}`,
-            iconURL: e.interaction.user.avatarURL()
-        })
-        logger.addFields(
-            { name: 'Command', value: e.interaction.commandName, inline: true },
-            { name: 'Guild ID', value: e.guildId, inline: true },
-        )
-        logger.setFooter({ text: "Logger" })
-        channel.send({ embeds: [logger] })
+            const logger = new EmbedBuilder();
+            logger.setColor(MainColor);
+            logger.setAuthor({
+                name: `${e.interaction.user.username}#${e.interaction.user.discriminator}`,
+                iconURL: e.interaction.user.avatarURL()
+            });
+            logger.addFields(
+                { name: 'Command', value: e.interaction.commandName, inline: true },
+                { name: 'Guild ID', value: e.guildId, inline: true },
+            );
+            logger.setFooter({ text: "Logger" });
+            channel.send({ embeds: [logger] });
+        }
     }
 };
